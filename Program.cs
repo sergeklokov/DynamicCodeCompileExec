a365c6ed4ca1c3e84ab451a4c1f2c929286cdae2
sergeklokov <serge.klokov@gmail.com>
2018-11-05 21:28:43 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp; // install it from NuGet
+using Microsoft.CodeAnalysis.Emit;
+using System.CodeDom;
+using System.CodeDom.Compiler;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace DynamicCodeCompileExec
+{
+    /// <summary>
+    /// .Net Core Dynamic c# code compile and execute
+    /// by Serge Klokov
+    /// based on article below:
+    /// https://stackoverflow.com/questions/826398/is-it-possible-to-dynamically-compile-and-execute-c-sharp-code-fragments
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string codeToCompile =
+                @"
+                using System;
+
+                namespace RoslynCompileSample
+                {
+                    public class Writer
+                    {
+                        public void Write(string message)
+                        {
+                            Console.WriteLine(message);
+                            Console.WriteLine(""Please press any key.."");
+                            Console.ReadKey();
+                        }
+                    }
+                }";
+
+            SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(codeToCompile);
+
+
+            string assemblyName = Path.GetRandomFileName();
+            var assemblyPath = Path.GetDirectoryName(typeof(object).Assembly.Location);
+            MetadataReference[] references = new MetadataReference[]
+            {
+                MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+                MetadataReference.CreateFromFile(typeof(Enumerable).Assembly.Location),
+                MetadataReference.CreateFromFile(Path.Combine(assemblyPath, "System.Private.CoreLib.dll")),
+                MetadataReference.CreateFromFile(Path.Combine(assemblyPath, "System.Console.dll")),  // need it for Console.WriteLine function
+                MetadataReference.CreateFromFile(Path.Combine(assemblyPath, "System.Runtime.dll"))
+            };
+
+            CSharpCompilation compilation = CSharpCompilation.Create(
+                assemblyName,
+                syntaxTrees: new[] { syntaxTree },
+                references: references,
+                options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+            using (var memoryStream = new MemoryStream())
+            {
+                EmitResult result = compilation.Emit(memoryStream);
+
+                if (result.Success)
+                {
+                    memoryStream.Seek(0, SeekOrigin.Begin);
+                    Assembly assembly = Assembly.Load(memoryStream.ToArray());
+
+                    Type type = assembly.GetType("RoslynCompileSample.Writer");
+                    object obj = Activator.CreateInstance(type);
+                    type.InvokeMember("Write",
+                        BindingFlags.Default | BindingFlags.InvokeMethod,
+                        null,
+                        obj,
+                        new object[] { "Hello World from .Net Core compiled at runtime by Serge Klokov!" });
+                }
+                else
+                {
+                    IEnumerable<Diagnostic> failures = result.Diagnostics.Where(diagnostic =>
+                        diagnostic.IsWarningAsError ||
+                        diagnostic.Severity == DiagnosticSeverity.Error);
+
+                    foreach (Diagnostic diagnostic in failures)
+                    {
+                        Console.Error.WriteLine("{0}: {1}", diagnostic.Id, diagnostic.GetMessage());
+                    }
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Please press any key to exit main application..");
+            Console.ReadKey();
+
+        }
+    }
+}
